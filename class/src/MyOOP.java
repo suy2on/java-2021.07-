447e06ae82e7988f5fa05613319580a6f10963fc
suy2on <tnwjdrms@naver.com>
2021-09-03 01:45:21 +0900

    method and class

---
+++ class/src/MyOOP.java
@@ -0,0 +1,31 @@
+class Print{
+    public static String delimiter = "";
+    public static void A(){
+        System.out.println(delimiter);
+        System.out.println("A");
+        System.out.println("A");
+    }
+    public static void B(){
+        System.out.println(delimiter);
+        System.out.println("B");
+        System.out.println("B");
+    }
+}
+
+public class MyOOP {
+
+    public static void main(String[] args) {
+        Print.delimiter = "_____";
+        Print.A();
+        Print.A();
+        Print.B();
+        Print.B();
+
+        Print.delimiter = "*****";
+        Print.A();
+        Print.A();
+        Print.B();
+        Print.B();
+
+    }
+}
